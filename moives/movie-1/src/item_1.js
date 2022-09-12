02b60b334b886200d2d5d2dc26620da51ec5fb3f
pcplusmac <feiloveit2017@gmail.com>
2022-09-12 18:23:28 +0800

    Update item_1.js
    add code for fetching the data from online api;
    add feature for validating the new data in render() return() before show the data on browser;
    the films are array type, in order to keep the code tidy, we add a variable to represent the whole block jsx code, then complete the jsx code for showing array data and assign to movies variable

--- moives/movie-1/src/item_1.js
+++ moives/movie-1/src/item_1.js
@@ -25,17 +25,47 @@ class ItemApi extends React.Component{
         // make url to be a variable using temple literal
         const url=`https://swapi.dev/api/people/${randomNum}`
         // fetch the data using the url; then set the state uisng these data.
+        fetch(url)
+            .then(response => response.json())
+            .then(data=>{
+                this.setState({
+                    name:data.name,
+                    birth_year: data.birth_year,
+                    homeworld: this.state.homeworld,
+                    films: data.films,
+                    // put a pair for verifying the newData coming
+                    newData:true
+                })
+                
+            })
         
-        this.setState()
     }
 
     // in render, implement the return statment so that the component can be use the all set state data to complete the jsx code for the browser as output; 
     // add button if there is need for button in component.  
-    // complete the feature of showing the updated state on browser in dynamic way. 
+    
     render(){
+        // assign jsx code to variable 'movies' using map()
+        const movies= this.state.films.map((url,i)=>{
+            return (
+                <FilmRow key={i} film={url} />
+
+        )})
         return(
             <div>
-                <ItemApi />
+                {
+                    // complete the feature of showing the updated state on browser in dynamic way. 
+                    this.state.newData && 
+                    <div>
+                        <h1>name:{this.state.name}</h1>
+                        <h1>birth_year: {this.state.birth_year}</h1>
+                        <h1>homeworld:<a href={this.state.homeworld}></a></h1>
+                        <h1> {movies}</h1>
+                    </div>
+                
+
+                
+                }
             </div>
         )
     }
